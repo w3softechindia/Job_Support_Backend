a75c6af03ad21927d6bfd4cdd4c358ffdb3bad16
Sri-Charan-0516 <sricharan824@gmail.com>
2024-08-04 07:58:30 +0530

    04/08/2024

--- src/main/java/com/example/JobSupportBackend/JobSupportBackendApplication.java
+++ src/main/java/com/example/JobSupportBackend/JobSupportBackendApplication.java
@@ -2,10 +2,12 @@ package com.example.JobSupportBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 
-public class JobSupportBackendApplication {
+public class JobSupportBackendApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(JobSupportBackendApplication.class, args);
